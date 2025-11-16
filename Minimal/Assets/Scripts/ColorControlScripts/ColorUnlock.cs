cce093ff5b195da26555ec9ad6e63d0cf80d9ded
SamuelSchnee <99436589+SamuelSchnee@users.noreply.github.com>
2025-11-16 09:49:08 -0800

    Added test world and started on color unlock ssytems

---
+++ Minimal/Assets/Scripts/ColorControlScripts/ColorUnlock.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class ColorUnlock : MonoBehaviour
+{
+    public GameObject colorcontroller;
+    public ColorController ColorCtr;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        colorcontroller = GameObject.FindGameObjectWithTag("ColorController");
+        ColorCtr = colorcontroller.GetComponent<ColorController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision Other)
+    {
+        if(Other.gameObject.tag == "Player")
+        {
+
+            Destroy(this.gameObject);
+        }
+    }
+}
